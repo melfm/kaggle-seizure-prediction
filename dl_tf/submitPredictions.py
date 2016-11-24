eab654db144fa6cfcec809069958e4bff8562648
mfm6 <melissafm24@gmail.com>
2016-11-24 18:05:17 -0500

    tiny bug with submission file writer

--- dl_tf/submitPredictions.py
+++ dl_tf/submitPredictions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import pdb
 
 path_to_all_datasets = os.path.abspath(os.path.join(
                         '..', 'data_dir/Kaggle_data/data'))
@@ -9,6 +10,7 @@ pred_dir = path_to_all_datasets + "/Pred_1/"
 test_files = os.listdir(pred_dir)
 
 for file_name in test_files:
+    if file_name.endswith(".csv"):
         df = pd.read_csv(pred_dir + file_name)
         df.columns = ['File', 'Class']
         submission_file = pd.merge(submission_file, df, how='left', on = ['File'], suffixes=['_1','_2'])
@@ -17,4 +19,3 @@ for file_name in test_files:
         submission_file.drop(['Class_1', 'Class_2'], axis=1, inplace=True)
 submission_file['Class'] = submission_file['Class']
 submission_file.to_csv(path_to_all_datasets + "/Submission_final.csv", index=False)
-
